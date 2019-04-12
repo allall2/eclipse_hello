5e465d8d8baf353d001340cbe04da97f2bf70c32
allall <allall@daegu.ac.kr>
2019-04-13 00:23:32 +0900

    Merge remote-tracking branch 'origin/master'
    conflicts:
    	Hello world/src/pushprint.java

--- Hello world/src/pushprint.java
+++ Hello world/src/pushprint.java
@@ -1,10 +1,7 @@
 public class pushprint{
 	public static void main(string[] args) {
 		System.out.println("Add java file");
-<<<<<<< HEAD
 		System.out.println("Hello in Local...");
-=======
 		System.out.println("Hello...");
->>>>>>> refs/remotes/origin/master
 	}
 }
